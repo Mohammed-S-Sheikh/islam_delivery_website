733967dcbf32e1604ead5b618f580fd94b7c26de
Mohammed-S-Sheikh <64172209+Mohammed-S-Sheikh@users.noreply.github.com>
2024-03-06 22:10:54 +0200

    Update authentication middleware in API routes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('admins', AdminController::class);
     Route::apiResource('cities', CityController::class);
